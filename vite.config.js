5f5a73a2775949f12951459d6d955ca33e59d44e
ndiyo <ndiyoedward@gmail.com>
2025-02-28 16:58:44 +0100

    Starting the project afresh with js this time with an attempt to make it more modular

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,3 @@ export default defineConfig({
 })
 
 
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   esbuild: {
-//     loader: 'jsx', // Ensures JSX is recognized
-//     include: /src\/.*\.js$/, // Applies only to JS files in src
-//   },
-// });
